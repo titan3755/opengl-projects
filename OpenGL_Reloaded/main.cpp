4cfa3e6bca207b5ebc97a326b9c119975ce37ca4
Titan <68808479+titan3755@users.noreply.github.com>
2025-07-26 16:36:46 +0600

    added shader manager to opengl_reloaded project

--- OpenGL_Reloaded/main.cpp
+++ OpenGL_Reloaded/main.cpp
@@ -1,24 +1,16 @@
+// ogl
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+// std
 #include <iostream>
+
+// local_headers
+#include <shader_manager.hpp>
 
 const int WIDTH = 800;
 const int HEIGHT = 600;
-const char* vertexShaderSource = "#version 460 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\0";
-const char* fragmentShaderSource = "#version 460 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"   FragColor = vec4(0.1, 0.9, 0.8, 1.0);\n"
-"}\0";
 
-// Function prototypes
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
 }
@@ -45,60 +37,16 @@ int main() {
     }
 
 	// shader compilation
-	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
-	glCompileShader(vertexShader);
-	int success;
-	char infoLog[512];
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
-        std::cerr << "Vertex Shader Compilation Failed: " << infoLog << std::endl;
-        glDeleteShader(vertexShader);
-        glfwDestroyWindow(window);
-        glfwTerminate();
-        return -1;
-    }
-	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
-	glCompileShader(fragmentShader);
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
-        std::cerr << "Fragment Shader Compilation Failed: " << infoLog << std::endl;
-        glDeleteShader(fragmentShader);
-        glDeleteShader(vertexShader);
-        glfwDestroyWindow(window);
-        glfwTerminate();
-        return -1;
-	}
-	unsigned int shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
-        std::cerr << "Shader Program Linking Failed: " << infoLog << std::endl;
-        glDeleteShader(fragmentShader);
-        glDeleteShader(vertexShader);
-        glDeleteProgram(shaderProgram);
-        glfwDestroyWindow(window);
-        glfwTerminate();
-        return -1;
-	}
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
+	ShaderManager shaderManager("shaders/vertex.glsl", "shaders/fragment.glsl");
 
-	// Set the viewport
 	glViewport(0, 0, WIDTH, HEIGHT);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     float vertices[] = {
         // positions
-        -0.5f, -0.5f, 0.0f, // bottom left
+        -0.5f, -0.7f, 0.0f, // bottom left
          0.5f, -0.5f, 0.0f, // bottom right
-         0.0f,  0.5f, 0.0f  // top
+         0.0f,  0.4f, 0.0f  // top
 	};
 
     // rendering setups
@@ -110,13 +58,10 @@ int main() {
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
-
-	// Use the shader program
 	glBindVertexArray(VAO);
-	glUseProgram(shaderProgram);
+	shaderManager.use();
 
-	// Print OpenGL information
-
+	// ogl info
 	std::cout << "OpenGL Version: " << glGetString(GL_VERSION) << std::endl;
 	std::cout << "GLSL Version: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
 	std::cout << "Vendor: " << glGetString(GL_VENDOR) << std::endl;
@@ -128,14 +73,15 @@ int main() {
         if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
             glfwSetWindowShouldClose(window, true);
         }
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClearColor(0.5f, 0.5f, 0.8f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-		glUseProgram(shaderProgram);
+		shaderManager.use();
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
     // Clean
     glfwDestroyWindow(window);
 	glfwTerminate();
---
+++ OpenGL_Reloaded/shaders/fragment.glsl
@@ -0,0 +1,6 @@
+#version 460 core
+out vec4 FragColor;
+void main()
+{
+	FragColor = vec4(0.5, 0.9, 0.4, 1.0);
+}
